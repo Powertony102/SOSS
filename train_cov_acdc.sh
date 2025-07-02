ec6163f3753fd4b6281428e2d896bdc26091abb9
powertony102 <powertony102@gmail.com>
2025-07-02 08:11:27 +0000

    Modify ACDC default minimum labelnum

--- train_cov_acdc.sh
+++ train_cov_acdc.sh
@@ -1,6 +1,6 @@
 python train_acdc_cov_dfp.py \
     --dataset_path /home/jovyan/work/medical_dataset/ACDC_processed \
-    --labelnum 7 \
+    --labelnum 5 \
     --use_dfp \
     --gpu 1 \
     --model corn2d \
